75a7069bd265bc48672d2d5831a0d6f8cd6faeb3
BilalAS173 <bilalahmedshariff2005@gmail.com>
2025-02-04 22:08:25 +0500

    Correction q2.cpp

--- OOP Lab 2/2.cpp
+++ OOP Lab 2/2.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 void* add (void* array) {
- int *arr=(int*)array;
+int *arr=(int*)array;
 for (int i=0;i<5;i++) {
 cout<<"Enter value "<<i+1<<":"<<endl;
 cin>>arr[i];
  }
-    int result=0;
+    int* result= new int;
+    *result=0;
     for(int i=0;i<5;i++){
-    result+=arr[i];
+    *result+=arr[i];
     }
-    void *ptr=(void*)&result;
-    return ptr;
+   // void *ptr=(void*)&result;
+    return (void*)result;
 }
 int main (int argc, char* argv[]) {
     //int array[5]={1,2,3,4,5};
