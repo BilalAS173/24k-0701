45f19aee7e598d630291e32328752e7bcb2f11b6
BilalAS173 <bilalahmedshariff2005@gmail.com>
2025-02-17 23:54:55 +0500

    Update h.w task 3.cpp

--- OOP Lab 4/h.w task 3.cpp
+++ OOP Lab 4/h.w task 3.cpp
@@ -1,23 +1,77 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class Invoice {
 private:
-    std::string partNumber;
-    std::string partDescription;
+    string partnumber;
+    string partdescription;
     int quantity;
-    double pricePerItem;
-
+    double priceperitem;
 public:
-    // Constructor
-    Invoice(std::string partNum, std::string partDesc, int qty, double price) 
-        : partNumber(partNum), partDescription(partDesc) {
-        quantity = (qty > 0) ? qty : 0;
-        pricePerItem = (price > 0.0) ? price : 0.0;
+    Invoice(string partNum, string partDesc, int qty, double price) 
+        : partnumber(partNum), partdescription(partDesc) {
+        if (qty > 0) {
+            quantity = qty;
+        } else {
+            quantity = 0;
+        }
+        if (price > 0.0) {
+            priceperitem = price;
+        } else {
+            priceperitem = 0.0;
+        }
     }
 
-    // Function to calculate invoice amount
     double getInvoiceAmount() const {
-        return quantity * pricePerItem;
+        return quantity * priceperitem;
+    }
+
+    string getPartNumber() const { 
+        return partnumber; 
+    }
+    string getPartDescription() const { 
+        return partdescription;
+     }
+    int getQuantity() const {
+         return quantity; 
+        }
+    double getPricePerItem() const { 
+        return priceperitem;
+     }
+
+    void setQuantity(int qty) {
+        if (qty > 0) {
+            quantity = qty;
+        } else {
+            quantity = 0;
+        }
+    }
+
+    void setPricePerItem(double price) {
+        if (price > 0.0) {
+            priceperitem = price;
+        } else {
+            priceperitem = 0.0;
+        }
     }
 };
+
+int main() {
+    Invoice invoice1("B456", "Hammer", 20, 16.80);
+
+    cout << "Invoice Details:" << endl;
+    cout << "Part Number: " << invoice1.getPartNumber() << endl;
+    cout << "Part Description: " << invoice1.getPartDescription() << endl;
+    cout << "Quantity: " << invoice1.getQuantity() << endl;
+    cout << "Price per Item: " << invoice1.getPricePerItem() << endl;
+
+    cout << "Total Invoice Amount is: " << invoice1.getInvoiceAmount() << endl;
+    invoice1.setQuantity(15);
+    invoice1.setPricePerItem(20.00);
+    cout << "Updated Invoice Details:" << endl;
+    cout << "Quantity: " << invoice1.getQuantity() << endl;
+    cout << "Price per Item: " << invoice1.getPricePerItem() << endl;
+    cout << "Total Invoice Amount: " << invoice1.getInvoiceAmount() << endl;
+
+}
